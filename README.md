480af6552f2039f99e2a7609fcfdd4c967164aed
febriliankr <febrilian.kr@gmail.com>
2021-03-22 01:37:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Web Authn Example (with Magic)
+
+This example uses [Magic](http://magic.link/) to allow sign-in using [WebAuthn](https://webauthn.io/). It currently only supports Google Chrome.
+
+## Demo
+
+[https://authn.vercel.app/](https://authn.vercel.app/)
+
+## Deploy Your Own
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Fvercel-solutions%2Fnextjs-web-authn&project-name=magic-webauthn&repository-name=magic-webauthn&demo-title=Magic%20WebAuthn&demo-url=https%3A%2F%2Fauthn.vercel.app)
---
+++ pages/_document.js
@@ -0,0 +1,40 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const meta = {
+      title: 'Web Authn – Next.js and Magic Example',
+      description: 'Login with Yubikey or TouchID on your Chrome browser.',
+      image: 'https://authn.vercel.app/og.png',
+    };
+
+    return (
+      <Html>
+        <Head>
+          <meta name="robots" content="follow, index" />
+          <meta name="description" content={meta.description} />
+          <meta property="og:site_name" content={meta.title} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:image" content={meta.image} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@vercel" />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:image" content={meta.image} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
